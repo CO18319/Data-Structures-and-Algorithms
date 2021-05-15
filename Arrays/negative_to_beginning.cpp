8a6d4c64f3d0294118cbe301a5678c992d645dbe
CO18319 <deepindersingh653@gmail.com>
2021-05-15 20:33:41 +0530

    Move all negative numbers to beginning and positive to end with constant extra sp

---
+++ Arrays/negative_to_beginning.cpp
@@ -0,0 +1,26 @@
+/*
+    Move all negative numbers to beginning and positive to end with constant extra space
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
+        cin>>arr[i];
+
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            if (i != j)
+                swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    for(int i=0; i < n; i++)
+        cout<<arr[i]<< " ";
+    delete[] arr;
+}
